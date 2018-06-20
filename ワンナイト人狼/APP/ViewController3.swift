331fc54361d71975c21c39f4472798af2c3c66cc
赤坂顕世 <akasakakensei@akasakakensei-no-MacBook-Pro.local>
2018-06-20 23:38:07 +0900

    プレイヤー設定2

---
+++ ワンナイト人狼/APP/ViewController3.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController3.swift
+//  APP
+//
+//  Created by 赤坂顕世 on 2018/06/19.
+//  Copyright © 2018年 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // 閉じるボタンがタップされた時
+    @IBAction func onTapCancel(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    // ポップアップの外側をタップした時にポップアップを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        var tapLocation: CGPoint = CGPoint()
+        // タッチイベントを取得する
+        let touch = touches.first
+        // タップした座標を取得する
+        tapLocation = touch!.location(in: self.view)
+        
+        let popUpView: UIView = self.view.viewWithTag(100)! as UIView
+        
+        if !popUpView.frame.contains(tapLocation) {
+            self.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+}
