a1e304ec1f14e87bd23c9c83759b2300c0705184
nishio <>
2018-06-24 00:48:36 +0900

    storyBoard分割

---
+++ OneNightWolf/APP/GameSetting.swift
@@ -0,0 +1,36 @@
+//
+//  GameSetting.swift
+//  APP
+//
+//  Created by 西尾俊輝 on 2018/06/24.
+//  Copyright © 2018年 myname. All rights reserved.
+//
+
+import UIKit
+
+class GameSetting: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackToView(segue: UIStoryboardSegue) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
